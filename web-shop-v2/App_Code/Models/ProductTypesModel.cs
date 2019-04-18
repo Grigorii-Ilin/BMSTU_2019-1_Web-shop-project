90d93acc98472a6b48b1aa580b8254e9a8399768
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-04-18 08:54:25 +0300

    db start

---
+++ web-shop-v2/App_Code/Models/ProductTypesModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_shop_v2.App_Code.Models {
+    public class ProductTypesModel {
+        public string InsertProductTypes(ProductTypes productTypes) {
+            try {
+                var db = new VegetableDBEntities();
+                db.ProductTypes.Add(productTypes);
+                db.SaveChanges();
+                return productTypes.Name + " was successfully inserted";
+            }
+            catch (Exception e) {
+
+                return "Error"+e;
+            }
+        }
+
+        public string UpdateProductTypes(int id, ProductTypes productTypes) {
+            try {
+                var db = new VegetableDBEntities();
+                var p = db.ProductTypes.Find(id);
+
+                p.Name = productTypes.Name;
+
+                db.SaveChanges();
+                return productTypes.Name + " was successfully updated";
+            }
+            catch (Exception e) {
+
+                return "Error" + e;
+            }
+        }
+
+        public string DeleteProductTypes(int id) {
+            try {
+                var db = new VegetableDBEntities();
+                var productTypes = db.ProductTypes.Find(id);
+
+                db.ProductTypes.Attach(productTypes);
+                db.ProductTypes.Remove(productTypes);
+
+                db.SaveChanges();
+                return productTypes.Name + " was successfully deleted";
+            }
+            catch (Exception e) {
+
+                return "Error" + e;
+            }
+        }
+    }
+}
---
+++ web-shop-v2/Pages/About.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="web_shop_v2.Pages.About" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        Колхоз &quot;Светлый путь&quot;</p>
+    <p>
+        тел. 123-45-67</p>
+</asp:Content>
