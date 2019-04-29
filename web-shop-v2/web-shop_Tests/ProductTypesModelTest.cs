60123aa24961740d6516f95140eeddaf9df21d4b
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-04-29 21:02:52 +0300

    nunit begin

---
+++ web-shop-v2/web-shop_Tests/ProductTypesModelTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_shop_v2;
+//using web_shop_v2
+
+namespace web_shop_v2_Test {
+    [TestFixture]
+    public class ProductTypesModelTest {
+        Cart cart = new Cart() {
+            Amount = 1.8M,
+            ClientId = clientId,
+            DatePurchased = DateTime.Now,
+            IsInCart = true,
+            ProductId = id
+        };
+    }
+}
---
+++ web-shop-v2_Test/ProductTypesModelTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using web_shop_v2
+
+namespace web_shop_v2_Test {
+    [TestFixture]
+    public class ProductTypesModelTest {
+        var cart = new Cart() {
+            Amount = amount,
+            ClientId = clientId,
+            DatePurchased = DateTime.Now,
+            IsInCart = true,
+            ProductId = id
+        };
+    }
+}
